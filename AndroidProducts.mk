4gitN/platform_device_samsung_trlte

trlte: AOSiP-ify

AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-$(LOCAL_DIR)/lineage_trlte.mk
+$(LOCAL_DIR)/aosip_trlte.mk

ghostwheel/android_device_samsung_trlte

AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-$(LOCAL_DIR)/lineage_trlte.mk
+$(LOCAL_DIR)/aosip.mk